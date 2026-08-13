62953f2a9f56d355814883522ed5d39b223f1fd2
Sayumi-Wijekoon <sayumiwijekoon@gmail.com>
2026-08-13 23:04:47 +0530

    third commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const API_BASE = 'https://your-api-domain.com/api'; 
+const API_BASE = 'https://api.freeprojectapi.com/api';
+
 const HEADERS = { 'Content-Type': 'application/json' };
 
  async function apiGet(path) {
@@ -14,4 +15,37 @@ const HEADERS = { 'Content-Type': 'application/json' };
         }); 
     return res.json();
  }
+
+ function showAlert(type, msg) {
+  document.getElementById('alert').innerHTML = `<div class="alert alert-${type}">${msg}</div>`;
+}
+
+
+ async function handleLogin() {
+    console.log("login...");
+    
+
+    const user = document.getElementById('username').value;
+    const pass = document.getElementById('password').value;
+
+    const res = await apiPost('/EmployeeApp/login',{
+      userName:user,
+       password:pass
+    });
+
+    if(res.result == true){
+        sessionStorage.setItem('user',JSON.stringify(res.data));
+        
+        document.getElementById('login-section').style.display='none';
+        document.getElementById('dashboard-section').style.display='block';
+
+        loadInitialData();
+
+
+    }else{
+        showAlert('danger',res.message || 'Login failed.Please try again.');
+
+    }
+
+}
 
